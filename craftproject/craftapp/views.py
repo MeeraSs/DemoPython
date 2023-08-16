578f004c93ddb3498d8c9eaeb107735f7e1f9412
MeeraSs <meeras8625@gmail.com>
2023-08-16 19:09:13 +0530

    added

---
+++ craftproject/craftapp/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'index.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def detail(request):
+    return HttpResponse("Details Section")
+
+
+def thanks(request):
+    return HttpResponse("Thanks ")
